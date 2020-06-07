8b50a2e67217e659530f587f57a7e0124a9ef616
Mahesh Mahajan <Mahesh Mahajan@DESKTOP-5MIBVLC>
2020-06-07 16:06:47 +0530

    Commit Masgter 2

--- Controllers/EmpController.cs
+++ Controllers/EmpController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Cors;
@@ -28,6 +30,24 @@ namespace TestApi.Controllers
             List<Employee> lstEmployee = new List<Employee>();
             lstEmployee.Add(new Employee() {EmployeeId ="1" ,Name ="Mahesh" });
             return Ok(lstEmployee); 
+        }
+
+
+
+        /// <summary>
+        /// Returns the list of employee
+        /// </summary>
+        ///<returns></returns>
+        [HttpGet]
+        [Route("api/v1/ConnectVM")]
+        public IHttpActionResult GetVmData()
+        {
+            HttpWebRequest webRequest =(HttpWebRequest) HttpWebRequest.Create("http://10.0.0.4/test.txt");
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream stream = webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            string text = streamReader.ReadToEnd();
+            return Ok(text);
         }
     }
 }
